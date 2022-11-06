9eaed87d2fb3c4af61d4031da0d5ede7b7553c22
Gina <36635815+ginac10@users.noreply.github.com>
2022-11-05 19:07:29 -0700

    Time: 318 ms (8.40%), Space: 18.6 MB (91.69%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = defaultdict(int) # {num: frequency}
+        for n in nums:
+            dic[n] += 1
+        freq = sorted(dic.values(), reverse=True)
+        ans = []
+        for i in range(0, k):
+            x = list(dic.keys())[list(dic.values()).index(freq[i])]
+            ans.append(x)
+            del dic[x]
+        return ans
