2cfb863bed05293a6dc03b10a00ef80522c45b33
Gina <36635815+ginac10@users.noreply.github.com>
2022-11-05 18:29:31 -0700

    Time: 266 ms (25.03%), Space: 17.7 MB (64.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {} # {"abc": ["bac", "bca"]}
+        if len(strs) == 0:
+            return [[""]]
+        elif len(strs) == 1:
+            return [strs]
+        else:
+            for word in strs:
+                if "".join(sorted(word)) in anagrams.keys():
+                    anagrams[''.join(sorted(word))].append(word)
+                else:
+                    anagrams[''.join(sorted(word))] = [word]
+        return anagrams.values()
