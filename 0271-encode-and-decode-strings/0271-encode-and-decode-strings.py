3a1b97e749090f51d0cf8500cc036ea62a8870e4
Gina <36635815+ginac10@users.noreply.github.com>
2022-11-08 15:44:49 -0800

    Time: 191 ms (18.72%), Space: 14.5 MB (21.52%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -5,21 +5,22 @@ class Codec:
         str = ""
         for s in strs:
             str += s + "라"
-        return str
+        return str[:len(str) - 1]
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        strs = []
-        temp = ""
-        for c in s:
-            if c != '라':
-                temp += c
-            else:
-                strs.append(temp)
-                temp = ""
-        return strs
+        return s.split("라")
+        #strs = []
+        #temp = ""
+        #for c in s:
+            #if c != '라':
+                #temp += c
+            #else:
+                #strs.append(temp)
+                #temp = ""
+        #return strs
         
 
 
