93735e9afee13fe05d505210d4da10ec2d6ba02a
Gina <36635815+ginac10@users.noreply.github.com>
2022-12-27 12:17:39 -0800

    Time: 169 ms (64.70%), Space: 18.4 MB (42.53%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic = {}
+        dic = defaultdict(list)#{}
         for s in strs:
             temp = [0]*26 #
             for letter in s:
@@ -8,11 +8,14 @@ class Solution:
                                                 # In Python, NOT int("a"), but ord("a")
                 temp[index] = temp[index]+1
                 ##temp = temp[:index] + str(int(temp[index])+1) + temp[index+1:] # Str doesn't support item reassignment
-            temp  = '#'.join(str(x) for x in temp) ## https://stackoverflow.com/questions/5618878/how-to-convert-list-to-string
-            if temp in dic:
-                dic[temp].append(s) #
-            else:
-                dic[temp] = [s]
+            temp  = '#'.join(str(x) for x in temp) ## https://stackoverflow.com/questions/5618878/how-to-convert-list-to-string ## Need token to split the numbers
+            
+            dic[temp].append(s) 
+                # No need to do this
+                    #if temp in dic:
+                    #    dic[temp].append(s) #
+                    #else:
+                    #    dic[temp] = [s]
         
         return dic.values() # This cleanly returns the result in []
                     #result = []
