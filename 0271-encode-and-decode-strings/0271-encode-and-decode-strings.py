dd3728dffa77d665224ff71841632e613a133fb5
Gina <36635815+ginac10@users.noreply.github.com>
2022-12-27 15:34:20 -0800

    Time: 75 ms (79.58%), Space: 14.5 MB (23.06%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,23 +2,30 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        sol = ""
+        result = ""
         for s in strs:
-            sol += str(len(s)) + "#" + s # You have to have "#" in case of ##s
-        return sol
+            result += str(len(s)) + "#" + s
+        return result
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        ans, i = [], 0
-        while i < len(s):
-            j = i
-            while s[j] != "#":
-                j += 1
-            num = int(s[i:j])
-            ans.append(s[j+1:j+1+num])
-            i = j + num + 1
-        return ans
+        result = []
+        
+        # Edge case: if # is > 1 digit...
+        while s:
+            upTo = s.index("#")
+            if upTo == -1:
+                return result
+            
+            take = int(s[0:upTo])
+            s = s[upTo+1:]
+            result.append(s[0:take])
+            s = s[take:]
+            
+        
+        return result
+        
 
 
 # Your Codec object will be instantiated and called as such:
